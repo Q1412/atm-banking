7ea93880763f0309448126d723228251e0d34fff
Q1412 <98614796+Q1412@users.noreply.github.com>
2024-03-27 22:28:29 -0700

    Add files via upload

---
+++ atm-system/client/src/routes/about.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class AboutUs extends Component {
+	render() {
+		return (
+			<div className="div-container">
+				<h1>About Us</h1>
+				<div id="group">
+					<p>CS 152 - Final Project</p>
+					<p>Spring 2024</p>
+					<p>-----</p>
+					<ul>
+						<li>Do Tran</li>
+						<li>Quyen Nguyen</li>
+					</ul>
+				</div>
+			</div>
+		)
+	}
+}
